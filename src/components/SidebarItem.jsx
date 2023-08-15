426cc3370f92916c9b259cd2685ce52427dd736c
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-08-15 21:42:02 +0900

    [FEAT] IconPack deleted

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -2,15 +2,11 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SidebarItem({ to, icon, text }) {
   return (
     <li>
-      <Link to={to}>
-        <FontAwesomeIcon icon={icon} />
-        {text}
-      </Link>
+      <Link to={to}>{text}</Link>
     </li>
   );
 }
