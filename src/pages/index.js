ee98a1d01cd00810e04404110ff03afd79dc5140
Haneol <haneolj@gmail.com>
2023-08-16 23:02:37 +0900

    [FIX] Fix route : ScheduledTodo -> ScheduledToDo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("/ScheduledTodo");
+    router.push("/ScheduledToDo");
   }, []);
 
   return null;
