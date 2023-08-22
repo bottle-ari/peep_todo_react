da837a127ff2e7fedec0b0cb7f9d2bd52cea872f
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-08-22 10:23:47 +0900

    [FEAT] Login page added

--- src/components/sidebar/profile_item.jsx
+++ src/components/sidebar/profile_item.jsx
@@ -3,12 +3,7 @@ import Image from "next/image";
 function ProfileField({ name, email }) {
   return (
     <div className="flex items-center p-4">
-      <Image
-        src="/images/profile_image.png"
-        alt="PROFILE"
-        width={50}
-        height={50}
-      />
+      <Image src="/images/logo.png" alt="PROFILE" width={50} height={50} />
       <div className="ml-3">
         <span className="block text-xl font-bold">{name}</span>
         <span className="block text-xs text-ptd-dark-grey">{email}</span>
