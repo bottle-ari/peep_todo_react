60e5f4fe0ec665ce7db5f8c187b2693b5e5af01d
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-09-05 20:59:18 +0900

    [FEAT] Google login, frontend server.

--- src/components/login/login_buttons.jsx
+++ src/components/login/login_buttons.jsx
@@ -1,7 +1,55 @@
-import React from "react";
-import Link from "next/link";
+// // login_buttons.jsx
+// import React from "react";
+// import Link from "next/link";
+// import { authorizationUrl } from "@/pages/api/google_oauth";
 
-function LonginButtons() {
+// function LonginButtons() {
+//   return (
+//     <div>
+//       <span className="block">
+//         <button>네이버 로그인</button>
+//       </span>
+//       <span className="block">
+//         <button>카카오 로그인</button>
+//       </span>
+//       <span className="block">
+//         <Link href={""}>
+//           {console.log()}
+//           <button>구글 로그인</button>
+//         </Link>
+//       </span>
+//       <span className="block">
+//         <button>로그인이 안되나요?</button>
+//       </span>
+//     </div>
+//   );
+// }
+
+// export default LonginButtons;
+
+// login_buttons.jsx
+import React, { useState, useEffect } from "react";
+
+function LoginButtons() {
+  const [googleAuthUrl, setGoogleAuthUrl] = useState(null);
+
+  useEffect(() => {
+    // 서버로부터 authorizationUrl을 가져오는 함수
+    const fetchAuthUrl = async () => {
+      const res = await fetch("/api/google_oauth");
+      const data = await res.json();
+      setGoogleAuthUrl(data.authorizationUrl);
+    };
+
+    fetchAuthUrl();
+  }, []);
+
+  const handleGoogleLogin = () => {
+    if (googleAuthUrl) {
+      window.location.href = googleAuthUrl;
+    }
+  };
+
   return (
     <div>
       <span className="block">
@@ -11,9 +59,7 @@ function LonginButtons() {
         <button>카카오 로그인</button>
       </span>
       <span className="block">
-        <Link href="https://ruminatetodo.com:8080/google_login/">
-          <button>구글 로그인</button>
-        </Link>
+        <button onClick={handleGoogleLogin}>구글 로그인</button>
       </span>
       <span className="block">
         <button>로그인이 안되나요?</button>
@@ -22,4 +68,4 @@ function LonginButtons() {
   );
 }
 
-export default LonginButtons;
+export default LoginButtons;
