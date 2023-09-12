47d1a71b420cd8a7f91496ae93d00f179b5e1925
yachae1101 <110226531+yachae1101@users.noreply.github.com>
2023-09-12 21:34:21 +0900

    [REFATOR] LoginButtons.

--- src/components/login/login_buttons.jsx
+++ src/components/login/login_buttons.jsx
@@ -1,32 +1,3 @@
-// // login_buttons.jsx
-// import React from "react";
-// import Link from "next/link";
-// import { authorizationUrl } from "@/pages/api/google_oauth";
-
-// function LonginButtons() {
-//   return (
-//     <div>
-//       <span className="block">
-//         <button>네이버 로그인</button>
-//       </span>
-//       <span className="block">
-//         <button>카카오 로그인</button>
-//       </span>
-//       <span className="block">
-//         <Link href={""}>
-//           {console.log()}
-//           <button>구글 로그인</button>
-//         </Link>
-//       </span>
-//       <span className="block">
-//         <button>로그인이 안되나요?</button>
-//       </span>
-//     </div>
-//   );
-// }
-
-// export default LonginButtons;
-
 // login_buttons.jsx
 import React, { useState, useEffect } from "react";
 
